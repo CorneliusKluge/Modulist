58bc0b0f6c6e60e3cb4f15789fbb52ff3deb0610
Falk <falke123@gmx.net>
2021-05-03 14:12:48 +0200

    comment module controller and model

--- Controllers/ModuleController.php
+++ Controllers/ModuleController.php
@@ -3,31 +3,33 @@
 namespace Modulist\Controllers;
 
 class ModuleController {
-    function __construct() {
-        ob_start();
-        include("Views/Module/ModuleTemplate.php");
-        $template = ob_end_clean();
+    function __construct() { // Method which gets triggered whenever an object of the class "ModuleController" is created
+        ob_start(); // Start output buffering
+        include("Views/Module/ModuleTemplate.php"); // Include the Template
+        $template = ob_end_clean(); // Get the content of the output buffer and stop output buffering
 
-        $template = sprintf($template,
-                            $this->getModuleAddView()
-                            );
+        // Fill the template with views
+        $template = sprintf(
+            $template,
+            $this->getModuleAddView() // Calls the method "getModuleAddView" and write its output/return value into the template
+        );
 
-        echo $template;
+        echo $template; // Output the template
     }
     
     function getModuleAddView() {
-        ob_start();
-        if(isset($_POST["module_add_submit"])) {
-
+        ob_start(); // Start output buffering
+        if(isset($_POST["module_add_submit"])) { // Check if form has been submitted
+            // Not written yet
         }
-        $output = ob_end_clean();
+        $output = ob_end_clean(); // Get the content of the output buffer and stop output buffering
 
-        ob_start();
-        include("Views/Module/ModuleAddView.php");
-        $view = ob_end_clean();
+        ob_start(); // Start output buffering
+        include("Views/Module/ModuleAddView.php"); // Include the View which contains the formular to create a new module
+        $view = ob_end_clean(); // Get the content of the output buffer and stop output buffering
 
-        $view = sprintf($view, $output);
+        $view = sprintf($view, $output); //  Insert the content of the variable $output into the view
 
-        return $view;
+        return $view; // Return the view
     }
 }
--- Models/ModuleModel.php
+++ Models/ModuleModel.php
@@ -6,26 +6,26 @@ use Modulist\Services\DatabaseService;
 
 class ModuleModel {
     static function getAllModules() {
-        $db = DatabaseService::getDatabaseObject();
+        $db = DatabaseService::getDatabaseObject(); // Get the database object which contains login information to access the database
 
-        $query = "SELECT * FROM modules";
-        $result = mysqli_query($db, $query);
+        $query = "SELECT * FROM modules"; // A SQL query
+        $result = mysqli_query($db, $query); // Run the sql query and save the result in the variable $result
 
-        return $result;
+        return $result; // Return the result
     }
     static function getModuleByID($id) {
         $db = DatabaseService::getDatabaseObject();
 
-        $id = mysqli_real_escape_string($db, $id);
+        $id = mysqli_real_escape_string($db, $id); // Mask the data to prevent sql injection
 
         $query = "SELECT * FROM modules WHERE id = $id";
         $result = mysqli_query($db, $query);
 
-        if($result->num_rows) {
-            $result = mysqli_fetch_object($result);
+        if($result->num_rows) { // Check if there are data records (in this case: if there is a module with the given id)
+            $result = mysqli_fetch_object($result); // Convert the object into a normal php-object, so you can write e.g. $result->id to get the id of the current data record
         }
         else {
-            $result = false;
+            $result = false; // Return false if there is are no data records (no module with the given id)
         }
         return $result;
     }
