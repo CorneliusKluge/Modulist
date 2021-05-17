ca1987e5630868883eb7c74dafd35bc89978d592
Cornelius <32713780+Natsurr@users.noreply.github.com>
2021-05-17 14:33:51 +0200

    Add fonts and icons

--- Views/Category/CategoryTemplate.php
+++ Views/Category/CategoryTemplate.php
@@ -3,6 +3,20 @@
     <head>
     </head>
     <body>
+    <header>
+            <a class="img-logo"></a>
+            <nav class="navbar">
+                <ul class="navbar_container">
+                    <li class="navbar_entry"><a href="index.php?subcontroller=module">Module</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=category">Modulkategorien</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=literature">Literatur</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=studymanagement">Studienverwaltung</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=export">Export</a></li>
+                    <!-- <li class="navbar_entry"><a  href="index.php?subcontroller=options">Optionen</a></li> -->
+                </ul>
+            </nav>
+        </header>
+        <h1>Modulkategorienverwaltung</h1>
         %1$s
 
         %2$s
--- Views/StudyManagement/StudyManagementTemplate.php
+++ Views/StudyManagement/StudyManagementTemplate.php
@@ -5,6 +5,20 @@
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
+        <header>
+            <a class="img-logo"></a>
+            <nav class="navbar">
+                <ul class="navbar_container">
+                    <li class="navbar_entry"><a href="index.php?subcontroller=module">Module</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=category">Modulkategorien</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=literature">Literatur</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=studymanagement">Studienverwaltung</a></li>
+                    <li class="navbar_entry"><a href="index.php?subcontroller=export">Export</a></li>
+                    <!-- <li class="navbar_entry"><a  href="index.php?subcontroller=options">Optionen</a></li> -->
+                </ul>
+            </nav>
+        </header>
+        <h1>Studienverwaltung</h1>
         <h2>Studiengänge verwalten</h2>
         <div id="course_form_container"></div>
         
