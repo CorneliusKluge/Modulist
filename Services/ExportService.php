0d6b1dde8b2cf47ca0c5b403008a135aa86962a5
Falk <falke123@gmx.net>
2021-05-21 11:41:47 +0200

    add manual template

--- Services/ExportService.php
+++ Services/ExportService.php
@@ -9,10 +9,16 @@ class ExportService {
     static function exportModuleManual($courseID, $lang) {
         if($lang == "de") {
             ob_start();
+            include("Views/Services/Export/ModuleManual/ManualTemplate.php");
+            $template = ob_get_clean();
+
+            ob_start();
             $result = ModuleModel::getAllModulesOfCourse($courseID);
             include("Views/Services/Export/ModuleManual/IntroView.php");
             include("Views/Services/Export/ModuleManual/ModuleView.php");
             $view = ob_get_clean();
+
+            $template = sprintf($template, $view);
 
             ExportService::exportFile($view, "Modulhandbuch");
         }
---
+++ Views/Services/Export/ModuleManual/ManualTemplate.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+    </head>
+    <body>
+        %s
+    </body>
+</html>
