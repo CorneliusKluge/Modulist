d1093da15dd0a55eff763cb72a7e8863ac53b1e6
Cornelius <32713780+Natsurr@users.noreply.github.com>
2021-05-30 21:15:31 +0200

    Add export CSS

--- Views/Export/ExamScheduleExportView.php
+++ Views/Export/ExamScheduleExportView.php
@@ -2,20 +2,21 @@
     <div class="exam_schedules_container_header">
         <h2>Prüfungsplan exportieren</h2>
     </div>
-    <form method="POST">
-        <label for="export_examSchedule_course">Studiengang wählen</label>
-        <select id="export_examSchedule_course" name="export_examSchedule_course">
-            <?php
-            if($result->num_rows) {
-                foreach($result as $course) {
-                ?>
-                    <option value="<?php echo $course["ID"];?>"><?php echo $course["name"];?></option>
+    <form method="POST" class="export_form_container">
+        <div class="form_item">
+            <label class="form_label" for="export_examSchedule_course">Studiengang wählen</label>
+            <select class="form_select" id="export_examSchedule_course" name="export_examSchedule_course">
                 <?php
+                if($result->num_rows) {
+                    foreach($result as $course) {
+                    ?>
+                        <option value="<?php echo $course["ID"];?>"><?php echo $course["name"];?></option>
+                    <?php
+                    }
                 }
-            }
-            ?>
-        </select>
-
-        <button type="submit" name="export_examSchedule_submit">Exportieren</button>
+                ?>
+            </select>
+        </div>
+        <button type="submit" name="export_examSchedule_submit" class="button form_submit">Exportieren</button>
     </form>
 </div>
