ab14c5b9764c9473017e3f7d060961e0d216df17
Falk <falke123@gmx.net>
2021-05-31 16:24:19 +0200

    update database scheme

--- modulist.sql
+++ modulist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 28. Mai 2021 um 16:08
+-- Erstellungszeit: 31. Mai 2021 um 16:23
 -- Server-Version: 10.4.13-MariaDB
 -- PHP-Version: 7.4.7
 
@@ -34,6 +34,18 @@ CREATE TABLE `categories` (
   `ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Daten für Tabelle `categories`
+--
+
+INSERT INTO `categories` (`name`, `presenceFlag`, `position`, `ID`) VALUES
+('Vorlesung/Seminar', 1, 1, 1),
+('Übungen am Computer', 1, 2, 2),
+('Prüfungsleistung', 1, 3, 3),
+('Selbststudium/Übungen am Computer', 0, 1, 4),
+('Selbststudium/Übungen am Computer in Praxisphase', 0, 2, 5),
+('Selbststudium', 0, 3, 6);
+
 -- --------------------------------------------------------
 
 --
@@ -51,7 +63,7 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`name`, `nameEN`, `ID`) VALUES
-('Informationstechnologie', NULL, 1);
+('Informationstechnologie', 'Information Technology', 1);
 
 -- --------------------------------------------------------
 
@@ -65,9 +77,18 @@ CREATE TABLE `exams` (
   `examDuration` int(11) DEFAULT NULL,
   `examCircumference` text DEFAULT NULL,
   `examPeriod` text NOT NULL,
+  `examSemester` int(11) DEFAULT NULL,
   `examWeighting` int(11) NOT NULL,
   `ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `exams`
+--
+
+INSERT INTO `exams` (`moduleID`, `examType`, `examDuration`, `examCircumference`, `examPeriod`, `examSemester`, `examWeighting`, `ID`) VALUES
+(1, 1, 120, NULL, '1', NULL, 100, 1),
+(2, 1, 120, NULL, '1', NULL, 100, 2);
 
 -- --------------------------------------------------------
 
@@ -87,7 +108,8 @@ CREATE TABLE `fields` (
 --
 
 INSERT INTO `fields` (`name`, `nameEN`, `courseID`, `ID`) VALUES
-('Medieninformatik', NULL, 1, 1);
+('Medieninformatik', 'Media Informatics', 1, 1),
+('Informationstechnik', 'Information engineering', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -151,7 +173,8 @@ CREATE TABLE `modules` (
 --
 
 INSERT INTO `modules` (`name`, `nameEN`, `code`, `summary`, `summaryEN`, `type`, `semester`, `duration`, `credits`, `usability`, `examRequirement`, `participationRequirement`, `studyContent`, `knowledgeBroadening`, `knowledgeDeepening`, `instrumentalCompetence`, `systemicCompetence`, `communicativeCompetence`, `responsibleName`, `responsibleEmail`, `lectureLanguage`, `frequency`, `media`, `basicLiteraturePreNote`, `basicLiteraturePostNote`, `deepeningLiteraturePreNote`, `deepeningLiteraturePostNote`, `overallGradeWeighting`, `presenceCreditHours`, `selfLearningCreditHours`, `ID`) VALUES
-('Imperative Programmierung', NULL, '3IM-IMPPR-10', 'Die  Studierenden  lernen  die  imperative  und  prozedurale  Herangehensweise  theoretisch  und  am  praktischen  Beispiel  kennen.  Voraussetzung  für  die  Implementation  ist  das  Verständnis  für  die  Erarbeitung eines Algorithmus zur Lösung eines praktischen Problems. Dazu werden Kenntnisse über grafische Hilfsmittel (Ablaufpläne, Struktogramme) für die Umsetzung vermittelt. Der sichere Umgang mit  den  Kontrollstrukturen  für  strukturierte  Programmierung  ist  die  Voraussetzung  für  die  weiteren  Module der Softwareentwicklung.', NULL, 'Pflichtmodul', 1, 1, 6, 'Studiengang Informationstechnologie', 'Laut aktueller Prüfungsordnung', 'Keine', 'Begriffsdefinitionen und -erläuterungen:-Algorithmus, Programm, Programmierung -Klassifikation der Programmiersprachen -Darstellungsformen Programmablaufpläne, StruktogrammeSyntaxbeschreibungen, erweiterte Backus-Naur-Formstrukturiertes Vorgehen bei der ProgrammentwicklungProzedurale Programmiersprache:- Eigenschaften, elementare und strukturierte Datentypen, Operatoren (arithmetische, Vergleichs-, logische, Bedingungs-), Zuweisungen- Kontrollstrukturen (switch, for, if, while, do while)- Funktionen, Call-by-Value und Call-by-Reference- Zeiger, Felder, Strukturen- Dateiarbeit, Präcompiler- Testen von Programmen- Programmprojekte, Fehlerbehandlung', 'Die  Studierenden  kennen  die  Grundelemente  sowie  die  Konzepte  von  Programmiersprachen.  Sie  verstehen die Grundprinzipien der imperativen und prozeduralen Programmierung.', 'Die  Studierenden  beherrschen  die  Beschreibung  eines  Algorithmus  in  einer  problemorientierten  prozeduralen   Programmiersprache   und   die   notwendigen   Arbeitsschritte   zur   Erstellung   eines   Anwendungsprogramms. ', 'Die Studierenden können Entwicklungsumgebungen einsetzen, um Programme zu implementieren. Sie kennen  die  Werkzeuge  der  einzelnen  Arbeitsschritte  zur  Programmerstellung  sowie  die  benötigten  Systemkomponenten und sind somit in der Lage, für spezielle Anwendungen Programme zu erstellen.', 'Die   Studierenden   können   die   Grundprinzipien   der   prozeduralen   Programmierung   in   eigenen   Programmen anwenden. Sie sind in der Lage, einfache Problemstellungen algorithmisch zu formulieren und  die  erarbeiteten  Algorithmen  nach  den  Regeln  der  strukturierten  Programmierung  mit  den  gegebenen Möglichkeiten der Programmiersprache umzusetzen.', 'Die Studierenden sind in der Lage, auftretende Probleme bei der Algorithmierung und Programmierung im Team zu gemeinsam zu lösen, die Ergebnisse zu erläutern, zu demonstrieren und zu verteidigen. Sie können erhaltene Hinweise zu ihrer Lösung einarbeiten.', 'Herr Prof. Dipl.-Math. Engelhardt', 'Informationstechnik@ba-dresden.de', 'Deutsch', 'Jährlich (Wintersemester)', 'Skripte und Übungsbeispiele des Lehrbeauftragten', 'Ausgewählte Kapitel aus:', NULL, NULL, NULL, NULL, NULL, NULL, 1);
+('Imperative Programmierung', NULL, '3IM-IMPPR-10', 'Die  Studierenden  lernen  die  imperative  und  prozedurale  Herangehensweise  theoretisch  und  am  praktischen  Beispiel  kennen.  Voraussetzung  für  die  Implementation  ist  das  Verständnis  für  die  Erarbeitung eines Algorithmus zur Lösung eines praktischen Problems. Dazu werden Kenntnisse über grafische Hilfsmittel (Ablaufpläne, Struktogramme) für die Umsetzung vermittelt. Der sichere Umgang mit  den  Kontrollstrukturen  für  strukturierte  Programmierung  ist  die  Voraussetzung  für  die  weiteren  Module der Softwareentwicklung.', NULL, 'Pflichtmodul', 1, 1, 6, 'Studiengang Informationstechnologie', 'Laut aktueller Prüfungsordnung', 'Keine', 'Begriffsdefinitionen und -erläuterungen:-Algorithmus, Programm, Programmierung -Klassifikation der Programmiersprachen -Darstellungsformen Programmablaufpläne, StruktogrammeSyntaxbeschreibungen, erweiterte Backus-Naur-Formstrukturiertes Vorgehen bei der ProgrammentwicklungProzedurale Programmiersprache:- Eigenschaften, elementare und strukturierte Datentypen, Operatoren (arithmetische, Vergleichs-, logische, Bedingungs-), Zuweisungen- Kontrollstrukturen (switch, for, if, while, do while)- Funktionen, Call-by-Value und Call-by-Reference- Zeiger, Felder, Strukturen- Dateiarbeit, Präcompiler- Testen von Programmen- Programmprojekte, Fehlerbehandlung', 'Die  Studierenden  kennen  die  Grundelemente  sowie  die  Konzepte  von  Programmiersprachen.  Sie  verstehen die Grundprinzipien der imperativen und prozeduralen Programmierung.', 'Die  Studierenden  beherrschen  die  Beschreibung  eines  Algorithmus  in  einer  problemorientierten  prozeduralen   Programmiersprache   und   die   notwendigen   Arbeitsschritte   zur   Erstellung   eines   Anwendungsprogramms. ', 'Die Studierenden können Entwicklungsumgebungen einsetzen, um Programme zu implementieren. Sie kennen  die  Werkzeuge  der  einzelnen  Arbeitsschritte  zur  Programmerstellung  sowie  die  benötigten  Systemkomponenten und sind somit in der Lage, für spezielle Anwendungen Programme zu erstellen.', 'Die   Studierenden   können   die   Grundprinzipien   der   prozeduralen   Programmierung   in   eigenen   Programmen anwenden. Sie sind in der Lage, einfache Problemstellungen algorithmisch zu formulieren und  die  erarbeiteten  Algorithmen  nach  den  Regeln  der  strukturierten  Programmierung  mit  den  gegebenen Möglichkeiten der Programmiersprache umzusetzen.', 'Die Studierenden sind in der Lage, auftretende Probleme bei der Algorithmierung und Programmierung im Team zu gemeinsam zu lösen, die Ergebnisse zu erläutern, zu demonstrieren und zu verteidigen. Sie können erhaltene Hinweise zu ihrer Lösung einarbeiten.', 'Herr Prof. Dipl.-Math. Engelhardt', 'Informationstechnik@ba-dresden.de', 'Deutsch', 'Jährlich (Wintersemester)', 'Skripte und Übungsbeispiele des Lehrbeauftragten', 'Ausgewählte Kapitel aus:', NULL, NULL, NULL, NULL, NULL, NULL, 1),
+('Algebra/Analysis', NULL, '3MI-MATHE-10', 'Ziel ist  die  Vermittlung  von  Grundkenntnissen  mathematischen  Arbeitens  sowohl  mit  Methoden  der  Diskreten    Mathematik    als    auch    der    Analysis,    um    ingenieurtechnische    Aufgabenstellungen    mathematisch  formulieren  und  lösen  zu  können.  Das  Modul  ist  Voraussetzung  für  die  Module „Naturwissenschaftliche   Grundlagen“,   „Bildverarbeitung   und   Druckvorstufe“   und   „Angewandte Mathematik“ und unterstützt die Wissensvermittlung in weiteren Modulen.', NULL, 'Pflichtmodul', 1, 1, 6, 'Studiengang Informationstechnologie', 'Laut aktueller Prüfungsordnung', 'Keine', '-Grundlagen von Logik und Mengenlehre-Zahlenbereiche (insbes. komplexe Zahlen und Zahlenkongruenzen)-Algebraische Strukturen  -Vektorräume  -Matrizen und Determinanten -Allgemeine lineare Gleichungssysteme -Unendliche Folgen und Reihen-Stetige Funktionen-Infinitesimalrechnung ein- und mehrstelliger Funktionen -Differenzialgleichungen', 'Die  Studierenden  lernen  die  „Sprache  der  Mathematik“  (Logik  und  Mengenlehre)  und  können  diese  verstehen.  Sie  erlernen  effiziente  Algorithmen  zur  Lösung  linearer  Gleichungssysteme  und  können  weitere Aufgabenstellungen der Linearen Algebra lösen.', 'Die  Studierenden  erhalten  einen  Überblick  über  die  Struktur  der  Zahlenbereiche.  Ferner  wird  ein  Grundverständnis  für  die  Vielfalt  weiterer  algebraischer  Strukturen  vermittelt.  Sie  verstehen  die  theoretischen  Grundlagen  zur  Lösung  linearer  Gleichungssysteme  (mögliche  Lösungsfälle  und  derenCharakterisierung). Nach einem Einblick in die Theorie der Differenzialgleichungen sind sie in der Lage, selbständig einfache Probleme der Modellierung dynamischer Vorgänge zu lösen.', 'Die Studierenden können mathematische Modelle zur Lösung von informationstechnischen Aufgaben anwenden.  Sie  erwerben  rechnerische  Fertigkeiten,  insbesondere  in  informatikrelevanten  Zahlen-bereichen und beim Lösen von linearen Gleichungssystemen.', 'Sie  entwickeln  die  Fähigkeit,  formal  ausgedrückte  Sachverhalte  anschaulich  zu  interpretieren  und  umgekehrt    konkrete    Situationen    formal    zu    beschreiben.    Die    Studierenden    sind    befähigt,    naturwissenschaftliche oder technische Problemstellungen adäquat zu modellieren und mathematisch zu behandeln. Der der Diskreten Mathematik innewohnende hohe Abstraktionsgrad erleichtert ihnen die Analyse  von  praktischen  Problemstellungen  und  die  Entwicklung  klar  strukturierter  Lösungen  im  Rahmen der Software-Entwicklung.', 'Die  Studierenden  können  gewonnene  Ergebnisse  interpretieren  und  diese  für  eine  sachgerechte Argumentation und Entscheidungsfindung nutzen.', 'Herr Prof. Dr. rer. nat. Gembris', 'daniel.gembris@ba-dresden.de', 'Deutsch', 'Jährlich (Wintersemester)', 'Aufgaben- und Foliensammlung; Formelsammlung; Übungsbeispiele des Lehrbeauftragten', 'Ausgewählte Kapitel aus:', NULL, NULL, NULL, NULL, NULL, NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -167,6 +190,21 @@ CREATE TABLE `module_category_mm` (
   `semester` int(11) DEFAULT NULL,
   `ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `module_category_mm`
+--
+
+INSERT INTO `module_category_mm` (`moduleID`, `categoryID`, `workload`, `theoryFlag`, `semester`, `ID`) VALUES
+(1, 1, 28, NULL, 1, 2),
+(1, 2, 30, NULL, 1, 3),
+(1, 3, 2, NULL, 1, 4),
+(1, 4, 30, 1, 1, 5),
+(1, 5, 90, 0, 1, 6),
+(2, 1, 88, NULL, 1, 7),
+(2, 3, 2, NULL, 1, 8),
+(2, 6, 40, 1, 1, 9),
+(2, 6, 50, 0, 1, 10);
 
 -- --------------------------------------------------------
 
@@ -186,7 +224,8 @@ CREATE TABLE `module_field_mm` (
 --
 
 INSERT INTO `module_field_mm` (`moduleID`, `fieldID`, `courseID`, `ID`) VALUES
-(1, 1, 0, 1);
+(1, 1, 0, 1),
+(2, NULL, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -267,7 +306,7 @@ ALTER TABLE `module_literature_mm`
 -- AUTO_INCREMENT für Tabelle `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT für Tabelle `courses`
@@ -279,13 +318,13 @@ ALTER TABLE `courses`
 -- AUTO_INCREMENT für Tabelle `exams`
 --
 ALTER TABLE `exams`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `fields`
 --
 ALTER TABLE `fields`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `literature`
@@ -297,19 +336,19 @@ ALTER TABLE `literature`
 -- AUTO_INCREMENT für Tabelle `modules`
 --
 ALTER TABLE `modules`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `module_category_mm`
 --
 ALTER TABLE `module_category_mm`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT für Tabelle `module_field_mm`
 --
 ALTER TABLE `module_field_mm`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT für Tabelle `module_literature_mm`
