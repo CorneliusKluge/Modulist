e7de7b68b4c3c156980b1f0984b61df9225fee84
RoveRoverson <Jonixd47@gmail.com>
2021-06-03 19:43:38 +0200

    add Module Style English version

--- Views/Services/Export/ModuleManual/ModuleViewEN.php
+++ Views/Services/Export/ModuleManual/ModuleViewEN.php
@@ -1,7 +1,28 @@
 <h1>Module Manual <?php echo $courseNameEN;?></h1>
 <style>
+    table {
+        border-collapse: collapse;
+        width: max-content;
+        font-family: Arial, Helvetica, sans-serif;
+    }
     td {
-        border: 1p solid black;
+        border: 1px solid black;
+        text-align: center;
+    }
+    h1{
+        padding-top: 30px;
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 20px;
+    }
+    .summary_align {
+        text-align: left;
+    }
+    .cell_style {
+        background-color: lightskyblue;
+    }
+    .heading_extra {
+        font-weight: bolder;
     }
 </style>
 <table>
@@ -10,12 +31,12 @@
 
     foreach($courseModules as $module) {
     ?>
-        <tr>
+        <tr class="cell_style">
             <td><?php echo $module["code"];?></td>
             <td colspan="2"><?php echo $module["nameEN"];?></td>
         </tr>
         <tr>
-            <td><?php echo $module["summaryEN"];?></td>
+            <td class="summary_align"><?php echo $module["summaryEN"];?></td>
             <td><?php echo $module["semester"];?>st Semester</td>
             <td><?php echo $module["credits"];?> ECTS</td>
         </tr>
@@ -25,13 +46,13 @@
         if(count($modules)) {
             $fieldName = FieldModel::getFieldByID($id)->nameEN;
             ?>
-            <tr>
+            <tr class="heading_extra">
                 <td colspan="3"><?php echo $fieldName;?></td>
             </tr>
             <?php
             foreach($modules as $module) {
             ?>
-                <tr>
+                <tr class="cell_style">
                     <td><?php echo $module["code"];?></td>
                     <td colspan="2"><?php echo $module["nameEN"];?></td>
                 </tr>
